9d34c4f97d528b97338352b5fa856666b25e5d0a
noorjahan220 <noorjahanmeem220@gmail.com>
2024-09-25 02:17:31 +0600

    add button js

--- js/home.js
+++ js/home.js
@@ -22,3 +22,14 @@ document.getElementById('donate-btn').addEventListener('click', function(){
     }
 
 })
+
+document.getElementById('history-btn').addEventListener('click', function(){
+    document.getElementById('history-section').classList.remove('hidden');
+    document.getElementById('card-section').classList.add('hidden');
+});
+
+document.getElementById('donation-btn').addEventListener('click', function(){
+    document.getElementById('history-section').classList.add('hidden');
+    document.getElementById('card-section').classList.remove('hidden');
+
+})
